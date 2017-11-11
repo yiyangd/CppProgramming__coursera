22094f1bf2146d611814d1525d61cd0176b6bc5b
YIYANG DONG <tonyyoung9838@gmail.com>
2017-11-11 11:17:02 -0800

    Add files via upload

---
+++ Week5__Inheritance/MemberWithSameName.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class base
+{
+    int j;
+public:
+    int i;
+    void func(){}
+};
+
+class derived: public base
+{
+public:
+    int i; //same name!
+    void access();
+    void func(){} //same name!
+};
+
+void derived::access()
+{
+    j = 5; //error! private member from base class only could be accessed by its public/friend function
+    
+    i = 5; // i from derived class
+    base::i = 5; // i from base class
+    func(); // from derived class
+    base:: func(); //from base class
+}
+
+
+int main(int argc, const char * argv[]) {
+    derived obj;
+    obj.i = 1;
+    obj.base::i = 1;
+    system("pause");
+    return 0;
+}
