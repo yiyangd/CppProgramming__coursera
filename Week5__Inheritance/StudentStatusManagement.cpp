7580f367682ebdc2319fb3d0b9f1a9b07e550441
YIYANG DONG <tonyyoung9838@gmail.com>
2017-11-11 10:46:43 -0800

    Add files via upload

---
+++ Week5__Inheritance/StudentStatusManagement.cpp
@@ -0,0 +1,72 @@
+//
+//  main.cpp
+//  week10
+//
+//  Created by 董亦杨 on 2017/11/10.
+//  Copyright © 2017年 董亦杨. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class CStudent
+{
+private:
+    string name;
+    string id;
+    char gender;
+    int age;
+public:
+    void PrintInfo();
+    void SetInfo(const string& name_, const string& id_,
+                 int age_, char gender_);
+    string Getname(){return name;}
+};
+
+class CUndergraduateStudent:public CStudent
+{
+private:
+    string department;
+public:
+    void QualifiedSchlarship(){
+        cout<<"qualified for schlarship"<<endl;
+    }
+    
+    void PrintInfo(){
+        CStudent::PrintInfo();   // calling function from base class
+        cout<<"Department:"<<department<<endl;
+    }
+    
+    void SetInfo(const string& name_,const string& id_,
+                 int age_, char gender_,const string& department_){
+        CStudent::SetInfo(name_,id_,age_,gender_);
+        department = department_; //private member variable
+    }
+};
+void CStudent::PrintInfo()
+{
+    cout<<"Name:"<<name<<endl;
+    cout<<"ID:"<<id<<endl;
+    cout<<"Age:"<<age<<endl;
+    cout<<"Gender:"<<gender<<endl;
+}
+
+void CStudent::SetInfo(const string& name_, const string& id_,
+                       int age_, char gender_)
+{
+    name = name_;
+    id = id_;
+    age = age_;
+    gender = gender_;
+}
+
+int main(int argc, const char * argv[]) {
+    CUndergraduateStudent s2;
+    s2.SetInfo("YiyangDong","001",19,'M',"Computer Science");
+    cout<<s2.Getname()<<" "; //calling function from base class;
+    s2.QualifiedSchlarship();
+    s2.PrintInfo();
+    system("pause");
+    return 0;
+}
