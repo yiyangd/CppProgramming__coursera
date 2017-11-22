b91a2c920c9eefade69889db937f183963359fe7
YIYANG DONG <tonyyoung9838@gmail.com>
2017-11-22 11:48:04 -0800

    Update 01-Basic_Concept.cpp

--- Week7_File-Template/01-Basic_Concept.cpp
+++ Week7_File-Template/01-Basic_Concept.cpp
@@ -1,32 +1,38 @@
 #include <iostream>
 #include <fstream>
+using namespace std;
 
+int main()
+{
 // Open the file -- Method one
 
-ofstream outFile("clients.dat", ios::out|ios::binary)
-
-/*
-outFile is ofstream object
-ios::out ==> Open for output operations.
-ios::app ==> All output operations are performed at the end of the file, 
-             appending the content to the current content of the file.
-ios::binary ==> Open in binary mode.
-*/ 
-
-// Open the file -- Method two
-
-ofstream fout;
-fout.open("test.out",ios::out|ios::binary);
-
-// Test if the file is opened!
-
-if(!fout) 
-{
-  cerr << "File open error!" << endl;
- }
-
-if( fout.is_open())
-{
-  fout << " This is a line";
-  fout.close();
+    ofstream outFile("clients.dat", ios::out|ios::binary)
+    
+    /*
+    outFile is ofstream object
+    ios::out ==> Open for output operations.
+    ios::app ==> All output operations are performed at the end of the file, 
+                 appending the content to the current content of the file.
+    ios::binary ==> Open in binary mode.
+    */ 
+    
+    // Open the file -- Method two
+    
+    ofstream fout;
+    fout.open("test.out",ios::out|ios::binary);
+    
+    // Test if the file is opened!
+    
+    if(!fout) 
+    {
+      cerr << "File open error!" << endl;
+     }
+    
+    if( fout.is_open())
+    {
+      fout << " This is a line";
+      fout.close();
+    }
+    
+    return 0;
 }
