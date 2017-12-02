a4975ec0fa614c79f1b0a28353988e8a29027f84
YIYANG DONG <tonyyoung9838@gmail.com>
2017-12-01 23:29:16 -0800

    Create 02-ReverseIterator.cpp

---
+++ Week8_STL/02-ReverseIterator.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  vector<int>v;
+  v.push_back(1);v.push_back(2);v.push_back(3);v.push_back(4);
+  vector<int>reverse_iterator r;
+  for (r = v.rbegin();f=v.begin();j++)
+    *j = 100;
+  for (i = v.begin(); i != v.end(); i++)
+  cout << *i <<" ,";
+  return 0;
+  }
